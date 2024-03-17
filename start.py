5d644532b6ffffbb9d6d5434e62a5a1837603772
Mark <65709134+zzd2001@users.noreply.github.com>
2024-03-17 21:20:29 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import os
 
 # python 环境配置
-os.chdir('/home/xlab-app-center')  # 这个目录应该是浦语官方的存放应用的目录，不需要你自己创建
+os.chdir('/home/xlab-app-center')  # 这个目录应该是浦语官方的存放应用的目录，不需要自己创建
 os.system('mkdir -p download/xtuner019')
 os.chdir('download/xtuner019')
 os.system('git clone -b v0.1.9 https://gitee.com/Internlm/xtuner')
